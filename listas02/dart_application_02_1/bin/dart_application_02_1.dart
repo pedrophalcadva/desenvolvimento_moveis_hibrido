96246c58df3cb34bc1f03cadbaa11a2b4d462fe3
Pedro Ph Alca D.VA <103332245+pedrophalcadva@users.noreply.github.com>
2023-09-03 18:08:03 -0300

    Delete listas02 directory

--- listas02/dart_application_02_1/bin/dart_application_02_1.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:math';
-
-void main(List<Strings> arguments){
-  var x = <int>[];
-  var y = <int>[];
-  
-}
--- listas02/dart_application_1/bin/dart_application_1.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> arguments){
-  var pares = <int>[];
-  var impares = <int>[];
-
-  for (var i = 1; i < 11; i++){
-    if ( i % 2)
-  }
-}
